f4b0812ac85f044f9d4ec935fd9ff861faf7d725
Gabriel <gabrielcardoso_stelo@hotmail.com>
2022-10-06 19:50:55 -0300

    Acréscimo de conteúdo

---
+++ DOM/introducao.js
@@ -0,0 +1,13 @@
+`
+
+O DOM é uma API disponibilizada nos browser que editar o que é mostrado na tela
+
+"Objeto: document"
+
+Alguns métodos desse objeto:
+        getElementById()
+        getElementsByTagName()
+        getElementsByClassName()
+        querySelector()
+        querySelectorAll()
+`;
