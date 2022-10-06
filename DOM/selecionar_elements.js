70f0b026392498c703e51bd051610a97f99ac9b8
Gabriel <gabrielcardoso_stelo@hotmail.com>
2022-10-06 20:19:51 -0300

    Alterações

---
+++ DOM/selecionar_elements.js
@@ -0,0 +1,5 @@
+document.getElementById("nomeDoId");
+document.querySelector("qualquer seletor do CSS, ID ou Classe");
+document.getElementsByClassName("nomeDaClasse");
+document.querySelectorAll("qualquer seletor do CSS, ID ou Classe");
+document.getElementsByTagName("nomeDaTag");
